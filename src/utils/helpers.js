76179111955153abdaa431082500091f343f122a
Bastien Robert <bastienrobert@outlook.com>
2019-09-20 14:10:51 +0200

    adding global and case insensitive to formatted link regex

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,8 +1,13 @@
-const URL_REGEX = /\<(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\>/gi
+const URL_REGEX = /(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/
+const URL_REGEX_WITH_CHEVRON = new RegExp(
+  /\</.source + URL_REGEX.source + /\>/.source,
+  'g',
+  'i'
+)
 
 module.exports = {
   getLinksFromString: function(string) {
-    return string.match(URL_REGEX).slice(1, -1) || []
+    return string.match(URL_REGEX_WITH_CHEVRON).slice(1, -1) || []
   },
   formatLinksArray: function(links) {
     return links.join(' ')
